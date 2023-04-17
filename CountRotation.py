b35e4326954d816ff94376f6aad3c90c10e07f3b
Ngwe Becky <ngwebecky96@gmail.com>
2023-04-17 04:06:13 +0100

    count the number of times a sorted list was rotated

--- CountRotation.py
+++ CountRotation.py
@@ -1,7 +1,8 @@
+import time
 def linear_count_rotation(number_list):
     position = 0
     while position < len(number_list):
-        if position > 0 and number_list(position) < number_list(position-1):
+        if position > 0 and number_list[(position)] < number_list[(position-1)]:
             return position
         position += 1
     return -1
@@ -10,15 +11,25 @@ def linear_count_rotation(number_list):
 def binary_count_rotation(number_list):
     # if the middle number is smaller than the last element of the range, then the answer on the left
     # if the middle number is greate than the last element of the range, then the answer on the right
-    low = 0
-    high = len(number_list)
-    mid = (low+high)/2
-    mid_num = number_list[mid]
-    while low < high:
+    low, high = 0, len(number_list)-1
+    
+    while low <= high:
+        mid = (low+high)//2
+        mid_num = number_list[mid]
+        #print("lo:", low, ", hi:", high, ", mid:", mid, ", mid_number:", mid_num)
         if mid > 0 and mid_num < number_list[mid-1]:
             return mid
-        elif mid_num < number_list[(len(number_list))]:
+        elif mid_num < number_list[(len(number_list)-1)]:
+            print(len(number_list))
             high = mid-1
         else:
             low = mid+1
     return -1
+
+num_list = [17,34,-1,6,9,12,15]
+start_time = time.time()
+print(linear_count_rotation(num_list))
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+print(binary_count_rotation(num_list))
+print("--- %s seconds ---" % (time.time()-start_time))
